9a7904b18454a92dc9f2f964f5b78859b18e28c6
email <116715779sz@gmail.com>
2023-02-14 14:04:30 +0800

    模型创建

---
+++ bookmanager/book/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+'''models需要继承 models.Model
+    系统会自动添加主键ID
+    字段：
+    字段名=models。类型
+    
+'''
+class Book(models.Model):
+    name = models.CharField(max_length=10)
+
+class Peopleinfo(models.Model):
+    person = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    #id
+# Create your models here.
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# 注册 /安装子应用
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #way 1 : add 'book'
+    #way 2 as following:
+    'book.apps.BookConfig'
 ]
 
 MIDDLEWARE = [
